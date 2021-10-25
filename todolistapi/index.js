58352078732ff349a3cbd7648cfb90c4b2877492
Vincent He <kandom1001@gmail.com>
2021-10-25 16:56:33 +1100

    Rev.1

---
+++ todolistapi/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const toDoList = [];
+
+app.use(express.json());
+
+app.post("/", (req, res) => {
+  toDoList.push(req.body);
+  res.send(toDoList);
+});
+
+app.get("/", (req, res) => {
+  res.send(toDoList);
+});
+
+app.get("/:id", (req, res) => {
+  const { id } = req.params;
+  res.send(toDoList.find((element) => element.id == id));
+});
+
+app.get("/", (req, res) => {
+  res.send(toDoList);
+});
+
+app.put("/:id", (req, res) => {
+  const { id, description, done } = req.body;
+  targetIndex = toDoList.findIndex((element) => element.id == id);
+  toDoList[targetIndex].description = description;
+  toDoList[targetIndex].done = done;
+  res.send(toDoList.find((element) => element.id == id));
+});
+
+app.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  targetIndex = toDoList.findIndex((element) => element.id == id);
+  toDoList.splice(targetIndex, 1);
+  res.send(`task no. ${id} deleted`);
+});
+
+app.listen(8888, () => console.log(`listening on 8888`));
